73357367cbfa8857bf9f3b098dfc896b2e6ddc90
Abdul Rahman Vakili <vakili@univention.de>
2023-11-07 17:21:43 +0100

    other initial commits

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png", width=600)
+
+with col2:
+    st.title("Abdul Rahman, Vakili")
+    content = """
+    Hi, I am Abdul Rahman Vakili! A Senior IT system engineer. 
+    I did my master studies and later Ph.D in network and operating systems. 
+    I enjoy software development and doing DevOps !     
+    """
+    st.info(content)
+    #st.write(content)
