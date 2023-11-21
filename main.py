b6ed04ffbcf596b05f7d2d648af2c7703ea02a5f
Abdul Rahman Vakili <vakili@univention.de>
2023-11-21 13:37:33 +0100

    added app description and links

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import streamlit as st
 
+#3rd party library to use csv data..
+import pandas
+#Note: if not use pandas, then will have to do like:
+#with open("data.csv") as file:
+#    content = file.read()
+#    print(content)
+
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png", width=600)
+    st.image("images/photo.png", width=400)
 
 with col2:
     st.title("Abdul Rahman, Vakili")
@@ -17,7 +24,23 @@ with col2:
     #st.write(content)
 
 content2 = """
-Bellow you can find some or my works
+Bellow you can find some cool projects to click start coding ....
 """
 st.write(content2)
 
+#col3, col4 = st.columns(2)
+#To make empty space between two columns:
+col3 , empty_col, col4 = st.columns([1.5,0.5,1.5])
+df = pandas.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df[:10].iterrows():  #first 10 rows
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/"+row["image"], width=200)
+        st.write(f"[Source Code]({row['url']})")
+
+with col4:
+    for index, row in df[10:].iterrows(): #last 10 rows
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"]) # generates the path to image: eg: images/1.png
